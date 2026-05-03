c7c495fd0e8a7617dec71b605cc2a4e14bcadd49
Muhammad Umer Amir <omaraamir89@gmail.com>
2026-05-03 22:15:30 +0500

    Backend learning

---
+++ my-backend/models/Project.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const projectSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true, // must have a name
+  },
+  tech: {
+    type: String,
+    required: true, // must have tech
+  },
+  description: {
+    type: String,
+    default: "No description provided",
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Create the model from the schema
+const Project = mongoose.model("Project", projectSchema);
+
+// Export it so server.js can use it
+module.exports = Project;
