7600d134481014f7af7378ad6bf3e669ae13395a
Muhammad Umer Amir <omaraamir89@gmail.com>
2026-05-07 23:52:54 +0500

    Job schema and job controller

---
+++ hirehub/server/controllers/jobController.js
@@ -0,0 +1,12 @@
+const Job = require("../models/Job");
+
+const getAllJobs = async (req, res) => {
+  try {
+    // get all jobs + populate poster info
+    // return 200 with jobs
+  } catch (error) {
+    // return 500 error
+  }
+};
+
+module.exports = { getAllJobs };
---
+++ hirehub/server/models/job.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const jobSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  company: { type: String, required: true },
+  location: { type: String, required: true },
+  salary: { type: String, required: true },
+  description: { type: String, required: true },
+  type: {
+    type: String,
+    enum: ["full-time", "part-time", "remote", "contract"],
+    default: "full-time",
+  },
+  skills: [{ type: String, required: true }],
+  postedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Job = mongoose.model("Job", jobSchema);
+
+module.exports = Job;
