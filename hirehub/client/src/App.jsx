32f4319050b5861d925a0b8806b179466dcec811
Muhammad Umer Amir <omaraamir89@gmail.com>
2026-05-09 23:41:24 +0500

    Day 4: Vite + React + Tailwind setup

---
+++ hirehub/client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-950 text-white">
+      <h1 className="text-4xl font-bold text-center pt-20 text-purple-400">
+        HireHub 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
