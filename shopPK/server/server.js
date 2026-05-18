56b149dc2c867ef9343bd0c581d6dcd29bba97bd
Muhammad Umer Amir <omaraamir89@gmail.com>
2026-05-18 22:58:34 +0500

    Ecommerce Store Backend structure and Schema setup

---
+++ shopPK/server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.json({ message: "Server is working" });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on ${process.env.PORT}`);
+});
