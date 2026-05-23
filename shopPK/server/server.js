5db7cc99f3fc5773d746e4c2f2ad6d72731eb066
Muhammad Umer Amir <omaraamir89@gmail.com>
2026-05-23 18:28:01 +0500

    Day 2: Auth controller + middleware + routes tested

--- shopPK/server/server.js
+++ shopPK/server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
 const connectDB = require("./config/db");
 
 dotenv.config();
@@ -10,6 +11,8 @@ app.use(express.json());
 app.use(cors());
 
 connectDB();
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "Server is working" });
