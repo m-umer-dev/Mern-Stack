a656b7e71b8a3839d49698041644bf8df8e10f3f
Muhammad Umer Amir <omaraamir89@gmail.com>
2026-06-04 23:55:52 +0500

    Day 01: Adding the authSlice code + axios setup

--- shopPK/client/src/store/cartSlice.js
+++ shopPK/client/src/store/cartSlice.js
@@ -1,22 +1,27 @@
-addToCart: (state, action) => {
-  const existing = state.items.find(item => item._id === action.payload._id);
-  if (existing) {
-    existing.quantity += 1;
-  } else {
-    state.items.push({ ...action.payload, quantity: 1 }); // add new
-  }
-  state.totalPrice = state.items.reduce(
-    (total, item) => total + item.price * item.quantity, 0
-  );
-},
+import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+  items: [],
+  cart: 0,
+};
 
-removeFromCart: (state, action) => {
-  state.items = state.items.filter(item => item._id !== action.payload);
-},
-
-
-clearCart: (state) => {
-  state.items = [];
-  state.totalPrice = 0;
-},
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const existing = state.items.find(
+        (items) => items._id === action.payload._id,
+      );
+      if (existing) {
+        existing.quantity += 1;
+      } else {
+        state.items.push({ ...action.payload, quantity: 1 });
+      }
+      state.totalPrice = state.items.reduce(
+        (total, item) => total + item.price * item.quantity,
+        0,
+      );
+    },
+  },
+});
