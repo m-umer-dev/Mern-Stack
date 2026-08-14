db12e40b5cb78b382509e6da03b921838e5e9f09
Muhammad Umer Amir <161680343+m-umer-dev@users.noreply.github.com>
2026-08-14 16:41:45 +0500

    Creating Frontend Structure

---
+++ BookStore-App/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-3xl font-bold underline bg-red-600 text-white">
+      App
+    </div>
+  );
+};
+
+export default App;
