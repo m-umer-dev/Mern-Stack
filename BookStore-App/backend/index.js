753bcbad3b05fd06716056c911517b4cfc736d9d
Muhammad Umer Amir <161680343+m-umer-dev@users.noreply.github.com>
2026-08-15 17:23:36 +0500

    Complete Book Store App Backend

--- BookStore-App/backend/index.js
+++ BookStore-App/backend/index.js
@@ -9,13 +9,15 @@ const app = express();
 
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["content-type"],
-  }),
-);
+app.use(cors());
+
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["content-type"],
+//   }),
+// );
 
 app.get("/", (req, res) => {
   return res.status(203).send("Welcome to Mern Stack Backend");
